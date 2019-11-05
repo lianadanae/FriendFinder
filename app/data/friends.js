548b3e39f012c47f866a0e132be138cc3199391c
lianadanae <lianabette@gmail.com>
2019-11-05 18:12:10 -0500

    fixed file path error

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,82 +1,82 @@
-var friendsArray = [
-    {
-        name: 'Rachel Green',
-        photo: 'https://www.tvovermind.com/wp-content/uploads/2018/04/7c5320c90e618031de6f7b17cffbc13b-640x427.jpg'
-        scores: [
+// var friendsArray = [
+//     {
+//         name: 'Rachel Green',
+//         photo: 'https://www.tvovermind.com/wp-content/uploads/2018/04/7c5320c90e618031de6f7b17cffbc13b-640x427.jpg'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Monica Geller',
-        photo: 'https://www.missmalini.com/wp-content/uploads/2016/06/monica-reading-mental-floss.png'
-        scores: [
+//     {
+//         name: 'Monica Geller',
+//         photo: 'https://www.missmalini.com/wp-content/uploads/2016/06/monica-reading-mental-floss.png'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Phoebe Buffay'
-        photo: 'https://www.thesun.co.uk/wp-content/uploads/2019/09/NINTCHDBPICT000523697076.jpg'
-        scores: [
+//     {
+//         name: 'Phoebe Buffay'
+//         photo: 'https://www.thesun.co.uk/wp-content/uploads/2019/09/NINTCHDBPICT000523697076.jpg'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Ross Geller'
-        photo: 'http://www4.pictures.zimbio.com/mp/3KaOXN5eW7Ll.jpg'
-        scores: [
+//     {
+//         name: 'Ross Geller'
+//         photo: 'http://www4.pictures.zimbio.com/mp/3KaOXN5eW7Ll.jpg'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Chandler Bing'
-        photo: 'https://i.pinimg.com/564x/1c/ae/a2/1caea20e681ce50e375d2524a66808c3.jpg'
-        scores: [
+//     {
+//         name: 'Chandler Bing'
+//         photo: 'https://i.pinimg.com/564x/1c/ae/a2/1caea20e681ce50e375d2524a66808c3.jpg'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Joey'
-        photo: 'http://cdn3.whatculture.com/images/2019/06/1e272c0b514b7051-600x338.jpg'
-        scores: [
+//     {
+//         name: 'Joey'
+//         photo: 'http://cdn3.whatculture.com/images/2019/06/1e272c0b514b7051-600x338.jpg'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Gunther'
-        photo: 'https://imgix.bustle.com/rehost/2016/9/13/9a1d97a3-900b-49a7-b984-e609d4578498.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70'
-        scores: [
+//     {
+//         name: 'Gunther'
+//         photo: 'https://imgix.bustle.com/rehost/2016/9/13/9a1d97a3-900b-49a7-b984-e609d4578498.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Marcel'
-        photo: 'https://vignette.wikia.nocookie.net/friends/images/f/f7/Marcelandross.jpg/revision/latest?cb=20080803123627'
-        scores: [
+//     {
+//         name: 'Marcel'
+//         photo: 'https://vignette.wikia.nocookie.net/friends/images/f/f7/Marcelandross.jpg/revision/latest?cb=20080803123627'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Janice'
-        photo: 'https://cdn.newsapi.com.au/image/v1/db86c900963adf54542c92a4059b76ae'
-        scores: [
+//     {
+//         name: 'Janice'
+//         photo: 'https://cdn.newsapi.com.au/image/v1/db86c900963adf54542c92a4059b76ae'
+//         scores: [
 
-        ]
-    },
+//         ]
+//     },
 
-    {
-        name: 'Mr. Heckles'
-        photo: 'https://imgix.bustle.com/rehost/2016/9/13/b445545f-f3f4-441e-8ef5-19d9cacf4efc.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70'
-        scores: [
+//     {
+//         name: 'Mr. Heckles'
+//         photo: 'https://imgix.bustle.com/rehost/2016/9/13/b445545f-f3f4-441e-8ef5-19d9cacf4efc.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70'
+//         scores: [
 
-        ]
-    },
-];
-module.exports = friendsArray;
+//         ]
+//     },
+// ];
+// module.exports = friendsArray;
