b5b8a85088e9a4555d4963944709cdaef9ae2920
lianadanae <lianabette@gmail.com>
2019-11-04 20:36:04 -0500

    finished html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// Dependencies
+var path = require('path');
+
+
+// Export HTML Routes
+module.exports = function(app) {
+
+  //HTML GET Requests 
+  //Survey Page
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+  });
+
+  //Home Page
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+
+};
+
