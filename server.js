c6280ffbccb90a18aeb255836156ccc497be7fec
lianadanae <lianabette@gmail.com>
2019-11-05 21:50:26 -0500

    Fixed eslint

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
+
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require('express');
+const path = require('path');
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
-//Points server to route files
-require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
-require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
+// Points server to route files
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log('App listening on PORT ' + PORT);
+});
