eb6bc04d87a4ae49eedb9bd203a4c70fd8971203
lianadanae <lianabette@gmail.com>
2021-07-08 23:09:36 -0600

    app working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,172 +1,171 @@
-var friendsArray = [{
-    name: 'Rachel Green',
-    photo: 'https://www.tvovermind.com/wp-content/uploads/2018/04/7c5320c90e618031de6f7b17cffbc13b-640x427.jpg',
-    scores: [
-      '4',
-      '2',
-      '5',
-      '3',
-      '1',
-      '2',
-      '1',
-      '1',
-      '1',
-      '1'
-    ]
-  },
+var friendsData = [{
+  name: 'Rachel Green',
+  photo: 'https://www.tvovermind.com/wp-content/uploads/2018/04/7c5320c90e618031de6f7b17cffbc13b-640x427.jpg',
+  scores: [
+    4,
+    2,
+    5,
+    3,
+    1,
+    2,
+    1,
+    1,
+    1,
+    1
+  ]
+},
 
-  {
-    name: 'Monica Geller',
-    photo: 'https://www.missmalini.com/wp-content/uploads/2016/06/monica-reading-mental-floss.png',
-    scores: [
-      '5',
-      '3',
-      '2',
-      '4',
-      '3',
-      '1',
-      '2',
-      '2',
-      '1',
-      '1'
-    ]
-  },
+{
+  name: 'Monica Geller',
+  photo: 'https://www.missmalini.com/wp-content/uploads/2016/06/monica-reading-mental-floss.png',
+  scores: [
+    5,
+    3,
+    2,
+    4,
+    3,
+    1,
+    2,
+    2,
+    1,
+    1
+  ]
+},
 
-  {
-    name: 'Phoebe Buffay',
-    photo: 'https://www.thesun.co.uk/wp-content/uploads/2019/09/NINTCHDBPICT000523697076.jpg',
-    scores: [
-      '1',
-      '4',
-      '1',
-      '2',
-      '2',
-      '5',
-      '2',
-      '1',
-      '2',
-      '1'
-    ]
-  },
+{
+  name: 'Phoebe Buffay',
+  photo: 'https://www.thesun.co.uk/wp-content/uploads/2019/09/NINTCHDBPICT000523697076.jpg',
+  scores: [
+    1,
+    4,
+    1,
+    2,
+    2,
+    5,
+    2,
+    1,
+    2,
+    1
+  ]
+},
 
-  {
-    name: 'Ross Geller',
-    photo: 'http://www4.pictures.zimbio.com/mp/3KaOXN5eW7Ll.jpg',
-    scores: [
-                  '1',
-                  '3',
-                  '1',
-                  '3',
-                  '1',
-                  '1',
-                  '5',
-                  '3',
-                  '2',
-                  '1'
+{
+  name: 'Ross Geller',
+  photo: 'http://www4.pictures.zimbio.com/mp/3KaOXN5eW7Ll.jpg',
+  scores: [
+    1,
+    3,
+    1,
+    3,
+    1,
+    1,
+    5,
+    3,
+    2,
+    1
+  ]
+},
 
-    ]
-  },
+{
+  name: 'Chandler Bing',
+  photo: 'https://i.pinimg.com/564x/1c/ae/a2/1caea20e681ce50e375d2524a66808c3.jpg',
+  scores: [
+    1,
+    4,
+    1,
+    1,
+    1,
+    1,
+    2,
+    5,
+    2,
+    1
+  ]
+},
 
-  {
-    name: 'Chandler Bing',
-    photo: 'https://i.pinimg.com/564x/1c/ae/a2/1caea20e681ce50e375d2524a66808c3.jpg',
-    scores: [
-      '1',
-      '4',
-      '1',
-      '1',
-      '1',
-      '1',
-      '2',
-      '5',
-      '2',
-      '1'
-    ]
-  },
+{
+  name: 'Joey Tribbiani',
+  photo: 'http://cdn3.whatculture.com/images/2019/06/1e272c0b514b7051-600x338.jpg',
+  scores: [
+    1,
+    5,
+    2,
+    1,
+    1,
+    3,
+    2,
+    2,
+    2,
+    1
+  ]
+},
 
-  {
-    name: 'Joey',
-    photo: 'http://cdn3.whatculture.com/images/2019/06/1e272c0b514b7051-600x338.jpg',
-    scores: [
-      '1',
-      '5',
-      '2',
-      '1',
-      '1',
-      '3',
-      '2',
-      '2',
-      '2',
-      '1'
-    ]
-  },
+{
+  name: 'Gunther',
+  photo: 'https://imgix.bustle.com/rehost/2016/9/13/9a1d97a3-900b-49a7-b984-e609d4578498.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70',
+  scores: [
+    2,
+    2,
+    1,
+    4,
+    5,
+    1,
+    1,
+    2,
+    3,
+    1
+  ]
+},
 
-  {
-    name: 'Gunther',
-    photo: 'https://imgix.bustle.com/rehost/2016/9/13/9a1d97a3-900b-49a7-b984-e609d4578498.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70',
-    scores: [
-      '2',
-      '2',
-      '1',
-      '4',
-      '5',
-      '1',
-      '1',
-      '2',
-      '3',
-      '1'
-    ]
-  },
+{
+  name: 'Marcel',
+  photo: 'https://vignette.wikia.nocookie.net/friends/images/f/f7/Marcelandross.jpg/revision/latest?cb=20080803123627',
+  scores: [
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    5
+  ]
+},
 
-  {
-    name: 'Marcel',
-    photo: 'https://vignette.wikia.nocookie.net/friends/images/f/f7/Marcelandross.jpg/revision/latest?cb=20080803123627',
-    scores: [
-                  '1',
-                  '1',
-                  '1',
-                  '1',
-                  '1',
-                  '1',
-                  '1',
-                  '1',
-                  '1',
-                  '5'
-    ]
-  },
+{
+  name: 'Janice',
+  photo: 'https://cdn.newsapi.com.au/image/v1/db86c900963adf54542c92a4059b76ae',
+  scores: [
+    1,
+    2,
+    4,
+    1,
+    1,
+    3,
+    1,
+    3,
+    5,
+    2
+  ]
+},
 
-  {
-    name: 'Janice',
-    photo: 'https://cdn.newsapi.com.au/image/v1/db86c900963adf54542c92a4059b76ae',
-    scores: [
-      '1',
-      '2',
-      '4',
-      '1',
-      '1',
-      '3',
-      '1',
-      '3',
-      '5',
-      '2'
-    ]
-  },
-
-  {
-    name: 'Mr. Heckles',
-    photo: 'https://imgix.bustle.com/rehost/2016/9/13/b445545f-f3f4-441e-8ef5-19d9cacf4efc.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70',
-    scores: [
-      '1',
-      '1',
-      '1',
-      '5',
-      '1',
-      '1',
-      '1',
-      '1',
-      '1',
-      '1'
-    ]
-  },
+{
+  name: 'Mr. Heckles',
+  photo: 'https://imgix.bustle.com/rehost/2016/9/13/b445545f-f3f4-441e-8ef5-19d9cacf4efc.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70',
+  scores: [
+    1,
+    1,
+    1,
+    5,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
+  ]
+}
 ];
-module.exports = friendsArray;
+module.exports = friendsData;
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 
 // Dependencies
 // =============================================================
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 // Sets up the Express App
 // =============================================================
-const app = express();
-const PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
 // Points server to route files
